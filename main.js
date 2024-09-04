ed892330c732b34e250d298c81fafd335e82d99e
pevincenty <124069797+pevincenty@users.noreply.github.com>
2024-09-04 13:30:13 -0300

    main.js

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-const openModal = document.querySelector('.btn_mdl')
-const modal = document.querySelector('.modal')
-const closeModal = document.querySelector('.modal_close')
+// Modal de contacto
+const openContactModal = document.querySelector('.btn_mdl');
+const contactModal = document.querySelector('.contact-modal');
+const contactForm = document.querySelector('.contact-modal form');
 
-var span = document.getElementsByClassName("close")[0];
-
-
-
-openModal.addEventListener('click', (e) => {
+openContactModal.addEventListener('click', (e) => {
     e.preventDefault();
-    modal.classList.add('modal--show')
-})
+    contactModal.style.display = 'block';
+    setTimeout(() => {
+        contactModal.classList.add('show');
+    }, 10);
+});
 
-closeModal.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.remove('modal--show')
-})
+// Manejo del evento de envío del formulario
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
 
-span.onclick = function () {
-    modal.classList.remove('modal--show')
-}
+    // Envía los datos del formulario utilizando fetch o XMLHttpRequest (ver ejemplos anteriores)
 
-//MODALES DE CARTAS
+    // Si el envío es exitoso:
+    // Muestra un mensaje de éxito al usuario
+    // Cierra el modal: 
+    contactModal.classList.remove('show');
+    setTimeout(() => {
+        contactModal.style.display = 'none';
+    }, 600);
+});
 
+// Modales de cartas
 var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
 var modal3 = document.getElementById("myModal3");
@@ -30,18 +35,14 @@ var btn1 = document.getElementById("myBtn1");
 var btn2 = document.getElementById("myBtn2");
 var btn3 = document.getElementById("myBtn3");
 
-
-var span1 = document.getElementsByClassName("close")[1];
-var span2 = document.getElementsByClassName("close")[2];
-var span3 = document.getElementsByClassName("close")[3];
-
-
+// Obtén todos los elementos con la clase "close" (incluyendo el del modal de contacto)
+var spansClose = document.querySelectorAll(".close"); 
 
 btn1.onclick = function () {
-    modal1.style.display = "block"; // Muestra modal
-    setTimeout(function () { // Aplica la clase 'show' después de un pequeño retraso
+    modal1.style.display = "block";
+    setTimeout(function () {
         modal1.classList.add('show');
-    }, 10); // Retraso de 10 milisegundos para asegurar que la transición se aplique
+    }, 10);
 }
 btn2.onclick = function() {
     modal2.style.display = "block";
@@ -57,46 +58,40 @@ btn3.onclick = function() {
     }, 10);
 }
 
+// Itera sobre todos los elementos con la clase "close"
+spansClose.forEach(function(span) {
+    span.onclick = function() {
+        // Encuentra el modal padre del span que se hizo clic
+        let modal = span.closest('.modal, .contact-modal'); 
 
-span1.onclick = function () {
-    modal1.classList.remove('show'); // Remueve la clase 'show'
-    setTimeout(function () { // Oculta la modal después de la transición
-        modal1.style.display = "none";
-    }, 600); // Retraso de 600 milisegundos
-}
+        modal.classList.remove('show');
+        setTimeout(function() {
+            modal.style.display = "none";
+        }, 600);
+    }
+});
 
-span2.onclick = function() {
-    modal2.classList.remove('show');
-    setTimeout(function() {
-        modal2.style.display = "none";
-    }, 600);
-}
-
-span3.onclick = function() {
-    modal3.classList.remove('show');
-    setTimeout(function() {
-        modal3.style.display = "none";
-    }, 600);
-}
-
-
+// Cierre de modales al hacer clic fuera del contenido
 window.onclick = function (event) {
     if (event.target == modal1) {
         modal1.classList.remove('show');
         setTimeout(function () {
             modal1.style.display = "none";
         }, 600);
-    }
-    if (event.target == modal2) {
+    } else if (event.target == modal2) {
         modal2.classList.remove('show');
         setTimeout(function () {
             modal2.style.display = "none";
         }, 600);
-    }
-    if (event.target == modal3) {
+    } else if (event.target == modal3) {
         modal3.classList.remove('show');
         setTimeout(function() {
-            modal2.style.display = "none";
+            modal3.style.display = "none";
         }, 600);
-    }
+    } else if (event.target == contactModal) { 
+        contactModal.classList.remove('show');
+        setTimeout(function() {
+            contactModal.style.display = "none";
+        }, 600);
+    } 
 }
